3350935cd777615618689b09b69a9b5e5f23c31e
Ewelina <ewelina.petelicka01@gmail.com>
2023-09-23 17:44:27 +0200

    header init

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import {Navigate, Route, Routes} from "react-router";
 import {Dashboard} from "./pages/dashboard/Dashboard";
+import {Header} from "./componetns/header/Header";
 
 export function App() {
     return (
-     <div>
-         <Routes>
-             <Route path={"/dashboard"} element={<Dashboard></Dashboard>}/>
-             <Route path={"*"} element={<Navigate to={'/dashboard'}></Navigate>}></Route>
-         </Routes>
-     </div>
-
+        <div className={'h-screen w-screen flex flex-col'}>
+            <Header></Header>
+            <div className={"grow bg-gray-900"}
+            ><Routes>
+                <Route path={"/dashboard"} element={<Dashboard></Dashboard>}/>
+                <Route path={"*"} element={<Navigate to={'/dashboard'}></Navigate>}></Route>
+            </Routes>
+            </div>
+        </div>
     );
 }
---
+++ src/componetns/header/Header.tsx
@@ -0,0 +1,12 @@
+export function Header (){
+    return(
+        <div className={"bg-sky-700 h-20 flex items-center text-3xl pl-10"}>
+            <button>
+                <h2 className={"text-blue-50 font-extrabold"}>
+                    <span className={"text-orange-400"}>MOVIE</span>
+                    PLEX
+                </h2>
+            </button>
+        </div>
+    )
+}
