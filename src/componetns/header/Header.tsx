29fe029d624afee0865d201035b54121679b3ba4
Ewelina <ewelina.petelicka01@gmail.com>
2023-10-01 21:36:17 +0200

    navigation to main page feature

--- src/componetns/header/Header.tsx
+++ src/componetns/header/Header.tsx
@@ -2,15 +2,17 @@ import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {faTicket, faBars} from '@fortawesome/free-solid-svg-icons';
 import {useState} from "react";
 import {Menu} from "../menu/Menu";
+import {useNavigate} from "react-router-dom";
 
 export function Header() {
     const [menu, setMenu] = useState<boolean>(false);
+    const navigate = useNavigate();
 
     return (
         <div className={"bg-sky-700 h-20 flex items-center text-3xl pl-10 pr-10 justify-between fixed w-full"}>
             <div>
                 <FontAwesomeIcon icon={faTicket} className={"rotate-45 pr-2 text-blue-50 "}/>
-                <button>
+                <button onClick={event => navigate("/dashboard")}>
                     <h2 className={"text-blue-50 font-extrabold"}>
                         <span className={"text-orange-400"}>MOVIE</span>
                         PLEX
