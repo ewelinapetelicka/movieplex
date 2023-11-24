cde4dc8e23b21206f7d9841cdc746752f19c4040
Ewelina <ewelina.petelicka01@gmail.com>
2023-11-24 16:10:45 +0100

    header

--- apps/management/src/components/core/header/Header.tsx
+++ apps/management/src/components/core/header/Header.tsx
@@ -1,5 +1,8 @@
 export function Header(){
     return(
-        <div>test</div>
+        <div className={"bg-primary h-5rem  flex justify-content-start align-items-center font-bold text-3xl pl-4 gap-2"}>
+            <i className="pi pi-ticket" style={{ fontSize: '2rem', rotate:'45deg'}}></i>
+            MOVIEPLEX
+        </div>
     )
 }
