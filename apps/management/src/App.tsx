03681156aa73ee83cd3e7d403753474edd43656e
Ewelina <ewelina.petelicka01@gmail.com>
2023-11-24 12:38:46 +0100

    header init

--- apps/management/src/App.tsx
+++ apps/management/src/App.tsx
@@ -2,14 +2,15 @@ import React from 'react';
 import {PrimeReactProvider} from 'primereact/api';
 import {Button} from "primereact/button";
 import {Route, Routes} from "react-router";
+import {Header} from "./components/core/header/Header";
 
 export function App() {
     return (
         <PrimeReactProvider>
-            <Routes>
-                <Route path={"/dashboard"} element={<Button></Button>}/>
-            </Routes>
+            <Header></Header>
+                <Routes>
+                    <Route path={"/dashboard"} element={<Button label={'test'}></Button>}/>
+                </Routes>
         </PrimeReactProvider>
-
     );
 }
