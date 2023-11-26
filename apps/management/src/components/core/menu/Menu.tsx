dec42c3c538d8988e85d47648a80bae1f3acf646
Ewelina <ewelina.petelicka01@gmail.com>
2023-11-26 21:49:33 +0100

    after refresh webiste the same menu page bug fix

--- apps/management/src/components/core/menu/Menu.tsx
+++ apps/management/src/components/core/menu/Menu.tsx
@@ -1,28 +1,30 @@
 import {ListBox, ListBoxChangeEvent} from 'primereact/listbox';
-import React, { useState } from "react";
-import {useNavigate} from "react-router";
+import React, {useState} from "react";
+import {useLocation, useNavigate} from "react-router";
 
 const options = [
-    { name: 'Film', nav: '/film' },
-    { name: 'Repertoire', nav: '/repertoire' },
-    { name: 'Hall', nav: '/hall' },
-    { name: 'Voucher', nav: '/voucher' }
+    {name: 'Film', nav: '/film'},
+    {name: 'Repertoire', nav: '/repertoire'},
+    {name: 'Hall', nav: '/hall'},
+    {name: 'Voucher', nav: '/voucher'}
 ];
 
-export function Menu(){
-    const [selectedOption, setSelectedOption] = useState<any>(null);
+export function Menu() {
+    const location = useLocation();
+    const [selectedOption, setSelectedOption] = useState<any>(options.find((el) => el.nav === location.pathname));
     const navigate = useNavigate();
 
-    function changeRoute(e: ListBoxChangeEvent){
-        if (e.value){
+    function changeRoute(e: ListBoxChangeEvent) {
+        if (e.value) {
             setSelectedOption(e.value);
             navigate(e.value.nav);
         }
     }
 
-    return(
+    return (
         <div className="card flex justify-content-center">
-            <ListBox value={selectedOption} onChange={(e) => changeRoute(e)} options={options} optionLabel="name" className="w-full md:w-10rem" />
+            <ListBox value={selectedOption} onChange={(e) => changeRoute(e)} options={options} optionLabel="name"
+                     className="w-full md:w-10rem"/>
         </div>
     )
 }
