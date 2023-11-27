7120e2125db197ec41e7d34eb9d690db36f063c3
Ewelina <ewelina.petelicka01@gmail.com>
2023-11-27 22:30:26 +0100

    add, delete voucher

--- apps/management/src/models/voucher/voucher.ts
+++ apps/management/src/models/voucher/voucher.ts
@@ -1,4 +1,6 @@
 export interface Voucher{
+    id: number,
     name: string,
     discount:number,
+
 }
--- apps/management/src/pages/voucher-page/VoucherPage.tsx
+++ apps/management/src/pages/voucher-page/VoucherPage.tsx
@@ -1,5 +1,8 @@
 import {useEffect, useState} from "react";
 import {Voucher} from "../../models/voucher/voucher";
+import {DataTable} from 'primereact/datatable';
+import {Column} from 'primereact/column';
+import {Button} from "primereact/button";
 
 export function VoucherPage() {
     const [vouchers, setVouchers] = useState<Voucher[]>([]);
@@ -12,17 +15,28 @@ export function VoucherPage() {
             })
     }, []);
 
+    function deleteVoucher(id: number) {
+        fetch('http://localhost:8000/vouchers/' + id, {
+            method: 'DELETE',
+        }).then(() => {
+            setVouchers(vouchers.filter((voucher) => voucher.id !== id));
+        });
+    }
+
     return (
         <div>
-            <div className={"w-full h-10 font-bold flex items-start"}>Vouchers</div>
-            <div className={"flex justify-evenly"}>{vouchers.map((el) => {
-                return (
-                    <div>
-                        <div>{el.name}</div>
-                        <div>{el.discount}</div>
-                    </div>
-                )
-            })}</div>
+            <div className={"w-full h-10 font-bold flex items-center "}>Vouchers</div>
+            <DataTable value={vouchers} tableStyle={{maxWidth: '90rem'}}>
+                <Column field="name" header="NAME"></Column>
+                <Column field="discount" header="DISCOUNT"></Column>
+                <Column header="ACTIONS" body={(voucher: Voucher) => {
+                    return (<div>
+                        <Button label={"Edit"} className={"p-button-success p-mr-2"}/>
+                        <Button label={"Delete"} className={"p-button-danger"} onClick={()=>{deleteVoucher(voucher.id)}}/>
+                    </div>)
+                }}>
+                </Column>
+            </DataTable>
         </div>
     );
 }
