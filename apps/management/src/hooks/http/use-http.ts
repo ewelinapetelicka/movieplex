085f1c994c2eb2a3790719ef71f2898705ee4c81
Ewelina <ewelina.petelicka01@gmail.com>
2023-12-12 18:22:48 +0100

    redirect get method

--- apps/management/src/hooks/http/use-http.ts
+++ apps/management/src/hooks/http/use-http.ts
@@ -2,8 +2,16 @@ export function useHttp() {
     const host = 'http://localhost:8000/';
 
     return {
-        get(endpoint: string) {
-            return fetch(host + endpoint).then((response) => response.json())
+        get(endpoint: string, params?: any) {
+            let url = host + endpoint;
+
+            if (params) {
+                url = url + "?";
+                for (const key in params) {
+                    url = url + key + "=" + params[key] + "&";
+                }
+            }
+            return fetch(url).then((response) => response.json())
         },
         delete(endpoint: string) {
             return fetch(host + endpoint, {
--- apps/management/src/pages/login-page/LoginPage.tsx
+++ apps/management/src/pages/login-page/LoginPage.tsx
@@ -1,7 +1,34 @@
+import {Card} from "primereact/card";
+import {Password} from "primereact/password";
+import {useState} from "react";
+import {InputText} from "primereact/inputtext";
+import {Button} from "primereact/button";
+import {useHttp} from "../../hooks/http/use-http";
+
 export function LoginPage(){
+    const [password, setPassword] = useState('');
+    const [email, setEmail] = useState('');
+    const http= useHttp();
+
+    function log(){
+        http.get("users", {
+            email:email,
+            password: password,
+        }).then((data) => {
+            console.log(data)
+        })
+    }
+
     return(
-        <div>
-            LoginPage
+        <div className={"h-screen w-full flex justify-content-center align-items-center"}>
+            <Card className={"w-8 h-30rem pl-4 gap-2 pr-4"} title={"Login"}>
+                <div className={"flex flex-column w-10 h-30rem gap-4"}>
+                    <InputText type="text" placeholder="email" className={"w-12"} value={email} onChange={(e)=>setEmail(e.target.value)}/>
+                    <Password value={password} onChange={(e) => setPassword(e.target.value)} inputClassName={"w-12"}
+                              toggleMask placeholder={"password"} />
+                    <Button label={"log in"} onClick={()=>log()} />
+                </div>
+            </Card>
         </div>
     )
 }
